216e5410e4bcf2ac7e217a684841e1c5042598c0
rubas3 <148117024+rubas3@users.noreply.github.com>
2023-12-29 15:12:12 -0800

    Final code for generation a weather report
    With the code iam adding 4 files.
    1) api_response.json (contains the raw data) 
    2)processed_data.json (contains the processed data)
    3)report_generating.txt (contains the weather report)
    4)headerFile.h (contains the declaration of functions)

---
+++ headerFile.h
@@ -0,0 +1,11 @@
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
+
+// Function declarations
+int retrieve_values();
+int processed_data(const char* dt_txt, cJSON* temp, cJSON* temp_min, cJSON* temp_max, cJSON* humidity);
+int calculated_data(const char* dt_txt, double *average_temp, int *lowest_temp, int *highest_temp, double *average_humidity);
+int processed_file_opening(const char* newdata);
+int report_generating();
+
+#endif   
