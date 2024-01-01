cddbb9ba6e9e62ea825e414853cf3af88ae8fa48
sidrafaruqi <137619786+sidrafaruqi@users.noreply.github.com>
2024-01-01 14:11:43 +0500

    Add files via upload

--- headerFile.h
+++ headerFile.h
@@ -4,7 +4,7 @@
 // Function declarations
 int retrieve_values();
 int processed_data(const char* dt_txt, cJSON* temp, cJSON* temp_min, cJSON* temp_max, cJSON* humidity);
-int calculated_data(const char* dt_txt, double *average_temp, int *lowest_temp, int *highest_temp, double *average_humidity);
+int calculated_data(const char* dt_txt, double *average_temp, double lowest_temp, double highest_temp, double *average_humidity);
 int processed_file_opening(const char* newdata);
 int report_generating();
 
